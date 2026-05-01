081053b422131633338d3319a741b4f18b5dbb70
yahooor <betamail@163.com>
2026-05-02 01:11:31 +0800

    fix: version 1.2.6b to 1.2.6b0 (PEP 440)

--- custom_components/zhangjiajie_water/const.py
+++ custom_components/zhangjiajie_water/const.py
@@ -2,4 +2,4 @@
 DOMAIN = "zhangjiajie_water"
 BASE_URL = "https://ccpay.thiscc.com"
 API_PATH = "/waterPay/search/searchRecord.action"
-INTEGRATION_VERSION = "1.2.6b"
+INTEGRATION_VERSION = "1.2.6b0"
